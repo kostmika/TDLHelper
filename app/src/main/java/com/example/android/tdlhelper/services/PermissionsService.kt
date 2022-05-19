4b3c3bd05a55890d9efbcd06dac1b4ab4bfd3b2c
mrsPepperPot <mrsPepperPot@gmail.com>
2022-05-19 16:50:01 +0300

    declared permission in manifest

---
+++ app/src/main/java/com/example/android/tdlhelper/services/PermissionsService.kt
@@ -0,0 +1,9 @@
+package com.example.android.tdlhelper.services
+
+import android.content.Context
+
+class PermissionsService private constructor(private var context: Context)  {
+    private var hasPermissions = false
+
+
+}
